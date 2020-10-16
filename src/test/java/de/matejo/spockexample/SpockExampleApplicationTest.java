28e5e58e9dd8031f448e10c4493bd2b1ca1c228b
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2020-10-16 20:53:46 +0200

    cleared dependencies for live coding

--- src/test/java/de/matejo/spockexample/SpockExampleApplicationTest.java
+++ src/test/java/de/matejo/spockexample/SpockExampleApplicationTest.java
@@ -3,7 +3,6 @@ package de.matejo.spockexample;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SpockExampleApplicationTest {
