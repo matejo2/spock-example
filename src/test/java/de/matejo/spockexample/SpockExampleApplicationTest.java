bb740e88e964f018783fd3ca2559dffc33b0d32a
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2020-10-16 19:54:08 +0200

    prepared live coding branch

---
+++ src/test/java/de/matejo/spockexample/SpockExampleApplicationTest.java
@@ -0,0 +1,14 @@
+package de.matejo.spockexample;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SpockExampleApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
