45edad8448e6f5430749afac50267856983c3a4a
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2020-10-16 17:55:50 +0200

    initial commit

---
+++ src/main/java/de/matejo/spockexample/SpockExampleApplication.java
@@ -0,0 +1,13 @@
+package de.matejo.spockexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpockExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpockExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/matejo/spockexample/SpockExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.matejo.spockexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpockExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
