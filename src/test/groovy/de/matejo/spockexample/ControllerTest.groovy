0ca4012b2f48869cacfc717efe8c64c42e477cd2
matejo <kixhot4Blau>
2022-04-17 20:39:06 +0200

    updated to currend spock version and implemented new mock style

--- src/test/groovy/de/matejo/spockexample/ControllerTest.groovy
+++ src/test/groovy/de/matejo/spockexample/ControllerTest.groovy
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.HttpStatus
 import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Ignore
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -19,6 +20,18 @@ class ControllerTest extends Specification {
     @SpringBean
     private Greeter greeter = Mock()
 
+    def "mock spy thing"() {
+        given:
+        1 * greeter.sayHelloTo("bla") >> "return this"
+        Controller controller = new Controller(greeter)
+
+        when:
+        def response = controller.sayHelloTo("bla")
+
+        then:
+        response == "return this"
+    }
+
     def "say hello"() {
         when: "uri is called"
         def response = mockMvc.perform(get("/hello")).andReturn().response
@@ -27,6 +40,21 @@ class ControllerTest extends Specification {
         response.status == HttpStatus.OK.value()
         and: "response is hello"
         response.getContentAsString() == "hello"
+    }
+
+    def "say hello to Foobar"() {
+        given:
+        def givenName = "foobar"
+        def expectedResponse = "hello foobar"
+        1 * greeter.sayHelloTo(givenName) >> expectedResponse
+
+        when: "url with name is called"
+        def response = mockMvc.perform(get("/hello/$givenName")).andReturn().response
+
+        then: "status code is 200 OK"
+        response.status == HttpStatus.OK.value()
+        and: "response is hello foobar"
+        response.getContentAsString() == expectedResponse
     }
 
     @Unroll
